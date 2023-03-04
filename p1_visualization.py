a0cb440615ac0462efc9d3f0b1c9204757f2fb8a
adeet1 <adeet.patel@gmail.com>
2023-03-04 16:52:14 -0500

    Extract temperature data for each station as it's loaded, instead of after all the files are loaded

--- p1_visualization.py
+++ p1_visualization.py
@@ -8,8 +8,7 @@ import time
 import collections
 
 # Data files
-start = time.time()
-files = sorted(glob('ghcnd_tiny/*.dly'))
+files = sorted(glob('ghcnd_small/*.dly'))
 
 # Load data about stations (we only the station ID and latitude)
 # that are in the northern hemisphere (a.k.a. latitude > 0)
@@ -24,37 +23,46 @@ north_stations = north_stations["latitude"].to_dict()
 # We only care about stations in the northern hemisphere (latitude > 0)
 # So go through the list of files, and only load files corresponding to
 # stations in the northern hemisphere.
-dataframes = []
+data_all = []
 for filename in tqdm.tqdm(files):
     # Get the station name from the filename
-    station_name = filename.split(sep='/')[1][:-4]
-    
+    station_name = filename.split(sep='/')[-1][:-4]
+
     # Get the latitude of the station if it's in the dictionary (if not, it's -1)
     latitude = north_stations.get(station_name, -1)
     
-    # Only load this file if the station is in the northern hemisphere
-    if latitude > 0:
-        station_data = pd.DataFrame.from_records(load_daily(filename))
-        dataframes.append(station_data)
+    # Only load this file if the station is in the northern hemisphere (a.k.a. having latitude > 0)
+    if latitude < 0:
+        continue
+    
+    # All the data for one station
+    df = pd.DataFrame.from_records(load_daily(filename))
+
+    # Extract the temperature data
+    filter_ = np.logical_or(df["element"] == "TMIN",
+                            df["element"] == "TMAX")
+    temperatures = df[filter_]
+    
+    # Delete unnecessary columns
+    temperatures = temperatures.drop(columns=["measurement", "quality", "source"])
+
+    data_all.append(temperatures)
+
+del filename, station_name, latitude, df, filter_, temperatures
+print("Reading files... DONE")
 
 # Combine all the dataframes
-df = pd.concat(dataframes)
-del dataframes
-print("Loaded the data in", time.time() - start, "seconds")
+data_all = pd.concat(data_all)
+print("Dataframe shape:", data_all.shape)
 
-# Extract the temperature data
-filter_ = np.logical_or(df["element"] == "TMIN",
-                        df["element"] == "TMAX")
-temperatures = df[filter_]
-
-# Delete missing data, and unnecessary columns
-temperatures = temperatures[temperatures["value"] != -9999]
-temperatures.drop(columns=["measurement", "quality", "source"], inplace=True)
-print(temperatures.groupby(by=["station_id"]).mean())
+# Delete missing data
+data_all = data_all[data_all["value"] != -9999]
+print(data_all.groupby(by=["station_id"]).mean())
 
 # For each station and for each day, compute the midpoint temperature by
 # averaging the min and max temperatures
-mid_temps = temperatures.groupby(by=["station_id", "year", "month", "day"]).mean().reset_index()
+print("Computing midpoint temperature...")
+mid_temps = data_all.groupby(by=["station_id", "year", "month", "day"]).mean().reset_index()
 
 # For each station and for each year, compute the average temperature across that year
 avg_yearly_temps_per_station = mid_temps.groupby(by=["station_id", "year"]).mean()["value"].reset_index()
@@ -69,3 +77,4 @@ avg_yearly_temps["value"] /= 10
 # Plot the result
 plt.plot(avg_yearly_temps["year"], avg_yearly_temps["value"])
 plt.show()
+plt.savefig("avg_yearly_temps.png")
