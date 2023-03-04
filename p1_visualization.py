52e406593cf3e7ed04aada27dece9ad981b884eb
adeet1 <adeet.patel@gmail.com>
2023-03-03 20:31:12 -0500

    Temperatures are in tenths of a degree Celsius

--- p1_visualization.py
+++ p1_visualization.py
@@ -23,6 +23,7 @@ temperatures = df[filter_]
 # Delete missing data, and unnecessary columns
 temperatures = temperatures[temperatures["value"] != -9999]
 temperatures.drop(columns=["measurement", "quality", "source"], inplace=True)
+print(temperatures.groupby(by=["station_id"]).mean())
 
 # For each station and for each day, compute the midpoint temperature by
 # averaging the min and max temperatures
@@ -33,6 +34,10 @@ avg_yearly_temps_per_station = mid_temps.groupby(by=["station_id", "year"]).mean
 
 # For each year, compute the average temperature across stations
 avg_yearly_temps = mid_temps.groupby(by=["year"]).mean()["value"].reset_index()
+
+# All temperatures are in tenths of degree Celsius, so divide by 10 to get
+# actual temperatures
+avg_yearly_temps["value"] /= 10
 
 # Plot the result
 plt.plot(avg_yearly_temps["year"], avg_yearly_temps["value"])
